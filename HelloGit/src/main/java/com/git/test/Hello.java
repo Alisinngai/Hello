650140608a9af123f0b707451e8b79b31bbebf9f
Alisinngai <1173441420@qq.com>
2021-04-02 13:19:37 +0800

    创建分支dev

--- HelloGit/src/main/java/com/git/test/Hello.java
+++ HelloGit/src/main/java/com/git/test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("测试git");
         System.out.println("更新1");
+        System.out.println("创建分支dev");
     }
 }
